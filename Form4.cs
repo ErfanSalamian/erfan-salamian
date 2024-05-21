666f0799b74d1c5583df1c8e60e501ffcbacb28a
ErfanSalamian <148633302+ErfanSalamian@users.noreply.github.com>
2024-05-21 12:08:20 +0330

    Add files via upload

---
+++ Form4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace erfan.salamian
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fs = File.Create("123.csv");
+            fs.Close();
+            string x;
+            x = textBox1.Text;
+            File.AppendAllText("123.csv",x + "\n");
+            textBox1.Clear();
+        }
+    }
+}
